1f90f9556d432ae8896a184ef032a375dfba4520
Thgiliwt <qiangqiang.wang@hotmail.com>
2021-05-22 02:59:41 +1000

    Have completed PyBank part. After the last update, it is found that the method I used wasn't performing 'DRY'. It is more convenient and efficient to set up some extra lists corresponded to the columns of the raw data file. I realised that with Python I could extend the issue to multi-dimensions, or simply adding more linear 1-d directions to make them into single parts. There are also indeed formatting tricks within 'the first one' and 'the position of the first one'of a list, range etc..

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,24 +7,53 @@ with open(pybank_path,'r') as bankcsv:
 
     csvreader = csv.reader(bankcsv, delimiter = ',')
 
-    print("Financial Analysis")
-    print("------------------------------")   
-    
-    row_count = len(list(csvreader)) - 1
-
-    bankcsv.seek(0)
-
     next(csvreader)
-    value_sum = 0
+
+    value_list = []
+    row_list = []
+    change_list = []
 
     for row in csvreader:
 
-        value_sum = value_sum + int(row[1])
+        value_list.append(int(row[1]))
+        row_list.append(row[0])
+
+
+    for i in range(1,len(value_list)):
+
+        change_list.append(value_list[i] - value_list[i-1])
+        change_avg = float(sum(change_list)/len(change_list))
+        change_max = max(change_list)
+        change_min = min(change_list)
+        change_max_date = str(row_list[change_list.index(change_max)+1])
+        change_min_date = str(row_list[change_list.index(change_min)+1])
+    
+    #Below is my initial method where due to the change of pointer position, I have to keep adding '.seek(0)' to replace it back to starting position.
+    
+    # row_count = len(list(csvreader)) - 1
+
+    # bankcsv.seek(0)
+
+    
+    # next(csvreader)
+    
+    # value_sum = 0
+    
+    # for row in csvreader:
+
+    #     value_sum = value_sum + int(row[1])
+
+    # bankcsv.seek(0)  
 
     
 
     
 
-    
-    print(f"Total Months: {row_count}")
-    print(f"Total: ${value_sum}")
+    print("Financial Analysis")
+    print("------------------------------")   
+    print(f"Total Months: {len(row_list)}")
+    print(f"Total: ${sum(value_list)}")
+    print("Average  Change: $%0.2f" %change_avg)
+    print(f"Greatest Increase in Profits: {change_max_date} (${change_max})")
+    print(f"Greatest Decrease in Profits: {change_min_date} (${change_min})")
+    bankcsv.close()
