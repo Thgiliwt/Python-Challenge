20d314586eb458d6cfd7b90d0c198d43fbd8bb27
Thgiliwt <qiangqiang.wang@hotmail.com>
2021-05-23 04:08:33 +1000

    Completed main script of PyRoll part. Few unsorted outcomes as: 1.could not print required outcome layout as a whole dict 2.trying to find out how to change the display of percentage 3.have not generated output text file yet

---
+++ PyRoll/main.py
@@ -0,0 +1,63 @@
+import csv
+import os
+from collections import Counter
+from contextlib import redirect_stdout
+
+pyroll_path = os.path.join('Resources','election_data.csv')
+
+with open(pyroll_path, 'r') as rollcsv:
+
+    csvreader = csv.reader(rollcsv,delimiter = ',')
+
+    next(csvreader)
+
+    voter_list = []
+    indiv_percent = []
+    candidate_count = []
+    #candidate_list = set()
+
+    
+
+    for row in csvreader:
+
+        voter_list.append(row[0])
+        candidate_count.append(row[2])
+
+        #candidate_list.add(row[2])
+    
+    vote_result = Counter(candidate_count)
+
+    values = vote_result.values()
+    values_list = list(values)
+
+    candidates = vote_result.keys()
+    candidates_list = list(candidates)
+
+    for i in range(0,len(values_list)):
+        indiv_percent.append(float(values_list[i]/len(voter_list)))
+
+
+    election = dict()
+
+    election["candidate"] = candidates_list
+    election["percentage"] = indiv_percent
+    election["vote"] = values_list
+    
+    #temp = '%(candidate)s, %(percentage)0.3f, (%(vote)i)'
+
+    max_vote = max(vote_result.values())
+    winner = [i for i in vote_result.keys() if vote_result[i] == max_vote]
+
+    # print(len(voter_list))
+    # print(candidates_list)
+    # print(indiv_percent)
+    # print(values_list)
+   
+    print("Election Results")
+    print("----------------------------")
+    print(f"Total Votes:  {len(voter_list)}")
+    print("----------------------------")
+    print(f"{candidates_list[0]}:  %0.3f  ({values_list[0]})" %indiv_percent[0])
+      
+    
+
