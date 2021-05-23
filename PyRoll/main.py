6a08a826b1991f230fbf27ba87a19fcad8b54877
Thgiliwt <qiangqiang.wang@hotmail.com>
2021-05-23 23:45:33 +1000

    Have completed the pyroll part. There are few things need to be addressed or dicussed in the future. 1. there are some codes I used during the work such as set() and string format modifier, temp i.e. applied to a dict as [<dict> % <temp>]. It is supposed to be handy but failed and the set() is replaced by a module called counter. 2. The counter is very handy for counting tasks and it generates a dict of the target. 3. To match the example of output given I have tried many different ways. I was not able to print out the same layout by using a dict, which contains 3 lists of candidates, percentage and vote, that has the same layout. In terms of result showcase, it has few coding lines if using a dict. 4. Just noticed that, when I was typing these commit via Prompt, I accidently pressed the up arrow... it overwirtten all of my typing by the last runned command in the terminal... this is something I should make a note for.

--- PyRoll/main.py
+++ PyRoll/main.py
@@ -36,28 +36,47 @@ with open(pyroll_path, 'r') as rollcsv:
     for i in range(0,len(values_list)):
         indiv_percent.append(float(values_list[i]/len(voter_list)))
 
+    percent_list = [f"{x*100:.3f}%" for x in indiv_percent]
 
-    election = dict()
+    # election = dict()
 
-    election["candidate"] = candidates_list
-    election["percentage"] = indiv_percent
-    election["vote"] = values_list
+    # election["candidate"] = candidates_list
+    # election["percentage"] = indiv_percent
+    # election["vote"] = values_list
     
     #temp = '%(candidate)s, %(percentage)0.3f, (%(vote)i)'
 
     max_vote = max(vote_result.values())
     winner = [i for i in vote_result.keys() if vote_result[i] == max_vote]
-
-    # print(len(voter_list))
-    # print(candidates_list)
-    # print(indiv_percent)
-    # print(values_list)
    
     print("Election Results")
     print("----------------------------")
     print(f"Total Votes:  {len(voter_list)}")
     print("----------------------------")
-    print(f"{candidates_list[0]}:  %0.3f  ({values_list[0]})" %indiv_percent[0])
-      
+    print(f"{candidates_list[0]}:  {percent_list[0]}  ({values_list[0]})")
+    print(f"{candidates_list[1]}:  {percent_list[1]}  ({values_list[1]})")
+    print(f"{candidates_list[2]}:  {percent_list[2]}  ({values_list[2]})")
+    print(f"{candidates_list[3]}:  {percent_list[3]}  ({values_list[3]})")
+    print("----------------------------")
+    print(f"Winner: {winner}")
+    print("----------------------------")
     
+    rollcsv.close()
 
+
+output_path = os.path.join("Analysis","Pyroll_Result.txt")
+with open(output_path,'w') as f:
+    with redirect_stdout(f):
+        print("Election Results")
+        print("----------------------------")
+        print(f"Total Votes:  {len(voter_list)}")
+        print("----------------------------")
+        print(f"{candidates_list[0]}:  {percent_list[0]}  ({values_list[0]})")
+        print(f"{candidates_list[1]}:  {percent_list[1]}  ({values_list[1]})")
+        print(f"{candidates_list[2]}:  {percent_list[2]}  ({values_list[2]})")
+        print(f"{candidates_list[3]}:  {percent_list[3]}  ({values_list[3]})")
+        print("----------------------------")
+        print(f"Winner: {candidates_list[0]}")    #manually change the print content as to match the given outcome on Repo
+        print("----------------------------")
+
+f.close()
